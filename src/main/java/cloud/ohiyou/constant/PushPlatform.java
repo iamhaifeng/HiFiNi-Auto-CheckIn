045994f17be6a6f909d651bca2ce2b1aaa461f0c
iamhaifeng <seawind97@126.com>
2026-05-19 17:31:01 +0800

    Update PushPlatform.java

--- src/main/java/cloud/ohiyou/constant/PushPlatform.java
+++ src/main/java/cloud/ohiyou/constant/PushPlatform.java
@@ -10,9 +10,7 @@ public enum PushPlatform {
     SERVER_CHAN("Server酱"),
     WECHAT_WORK("企业微信机器人"),
     DINGTALK("钉钉机器人"),
-    TELEGRAM("Telegram机器人"),
-    GOTIFY("Gotify"),
-    BARK("Bark");
+    TELEGRAM("Telegram机器人");
 
     private final String displayName;
 
